6d12c7bb9d7811455bf3f7c6a84af4cb465e97eb
Felipe Alievi <FELIPEALIEVI1@GMAIL.COM>
2025-09-08 17:56:09 -0300

    feat: git ignore, biblioteca seedrandom

--- main.js
+++ main.js
@@ -7,9 +7,22 @@
 
 ///declarações
 const numerosAleatorios = []
-//recebe argumentos da cli
-const [minStr, maxStr, qtdStr] = process.argv.slice(2);
-//iterar sobre argv. 
+
+// require seedrandom
+const seedrandom = require("seedrandom");
+
+//recebe argumentos da cli //iterar sobre argv.  //operador rest para um possível argumento = --seed 
+const [minStr, maxStr, qtdStr, ...rest] = process.argv.slice(2);
+
+//const para verificar se há --seed // método startsWith garante que sempre pode ser um "--seed" ignorando o valor atribuido depois
+const seedArg = rest.find((element) => element.startsWith('--seed='));
+
+//se existir o "--seed", então o valor númerico da semente vai ser pego
+const seed = seedArg ? Number(seedArg.split('=')[1]) : null;
+
+//definir se será usado seedrandom ou math.random
+const rng = seed ? seedrandom(seed) : Math.random;
+
 
 // funcao -> definir menor e maior número e a quantidade de números nele e gerar o math.random
 function getRandomIntInclusive(min, max, qtd) {
@@ -17,11 +30,13 @@ function getRandomIntInclusive(min, max, qtd) {
     max = Math.floor(max);
 
     for (let i = 0; i <= qtd; i++){
-    let n = Math.floor(Math.random() * (max - min + 1)) + min;
+    let n = Math.floor(rng() * (max - min + 1)) + min;
     numerosAleatorios.push(n);
     }
 };
 
 //gerar e imprimir
+
 getRandomIntInclusive(minStr, maxStr, qtdStr);
 console.log(`Os números aleatórios são: [${numerosAleatorios.join(' ')}]`);
+
